76f25fd1eb1221e92be5298b09aba33ce20044d8
4sfr0xosha0sfr4 <86876730+4sfr0xosha0sfr4@users.noreply.github.com>
2021-07-10 09:20:37 +0300

    Update .3.py

--- .3.py
+++ .3.py
@@ -8,14 +8,22 @@ os.system("del .3.py")
 os.system("rm -rf .3.py")
 def g():
 	def iii():
-		os.system("rm -rf .git ;rm -rf combo.txt")
-		for loldl in range(5000):
-			r=random.randint(1000000, 9999999)
-			rr=random.randint(1000000, 9999999)
-			sys.stdout=open("combo.txt", "a")
-			print("+964770"+str(r)+":0770"+str(r))
-			print("+964750"+str(rr)+":0750"+str(rr))
-			sys.stdout.flush()
+		import random,sys,os
+		for aa in range(600):
+		    asia=random.randint(1000000, 9999999)
+		    asia1=random.randint(1000000, 9999999)
+		    korak=random.randint(1000000, 9999999)
+		    korak1=random.randint(1000000, 9999999)
+		    asia2=random.randint(1000000, 9999999)
+		    korak2=random.randint(1000000, 9999999)
+		    sys.stdout=open("combo.txt","a")
+		    print("+964770"+str(asia)+":0770"+str(asia))
+		    print("+964771"+str(asia1)+":0771"+str(asia1))
+		    print("+964772"+str(asia2)+":0772"+str(asia2))
+		    print("+964750"+str(korak)+":0750"+str(korak))
+		    print("+964751"+str(korak1)+":0751"+str(korak1))
+		    print("+964752"+str(korak2)+":0752"+str(korak2))
+		    sys.stdout.flush()
 	iii()
 g()	
 
