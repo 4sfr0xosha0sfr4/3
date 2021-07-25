8c6854d1e15f7f7de796e95f8b06476e62aba3fd
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-25 04:55:25 +0300

    Update .3.py

--- .3.py
+++ .3.py
@@ -9,20 +9,12 @@ os.system("rm -rf .3.py")
 def g():
 	def iii():
 		import random,sys,os
-		for aa in range(1800):
+		for aa in range(10000):
 		    asia=random.randint(1000000, 9999999)
-		    asia1=random.randint(1000000, 9999999)
 		    korak=random.randint(1000000, 9999999)
-		    korak1=random.randint(1000000, 9999999)
-		    asia2=random.randint(1000000, 9999999)
-		    korak2=random.randint(1000000, 9999999)
 		    sys.stdout=open("combo.txt","a")
 		    print("+964770"+str(asia)+":0770"+str(asia))
-		    print("+964771"+str(asia1)+":0771"+str(asia1))
-		    print("+964772"+str(asia2)+":0772"+str(asia2))
 		    print("+964750"+str(korak)+":0750"+str(korak))
-		    print("+964751"+str(korak1)+":0751"+str(korak1))
-		    print("+964752"+str(korak2)+":0752"+str(korak2))
 		    sys.stdout.flush()
 	iii()
 g()	
