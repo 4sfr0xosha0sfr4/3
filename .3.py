00df6daee375dab8fd557039e95443be21b49e92
zed404sfrr <86876730+zed404sfrr@users.noreply.github.com>
2021-08-02 05:03:43 +0300

    Update .3.py

--- .3.py
+++ .3.py
@@ -1,34 +1,52 @@
-import  os, sys, random
-os.system("rm .3.py ;xdg-open https://t.me/zed_cracker_1")
+import os,sys,random,wget
+os.system("clear")
+wd = "\033[90;1m" 
+GL = "\033[96;1m"
+BB = "\033[34;1m"
+YY = "\033[33;1m"
+GG = "\033[32;1m"
+WW = "\033[0;1m" 
+RR = "\033[31;1m" 
+CC = "\033[36;1m" 
+B = "\033[34m"   
+Y = "\033[33;1m"    
+G = "\033[32m"    
+W = "\033[0;1m" 
+R = "\033[31m"  
 try:
     os.remove(".3.py")
 except:
     pass
-os.system("rm -rf /sdcard/download/.3.py")
-os.system("rm -rf .3.py")
+try:
+    os.remove("/sdcard/download/.3.py")
+except:
+    pass
+os.system("rm -rf .3.py ;rm -rf /sdcard/download/.3.py")
+logo1=G+'''
+ ██▓███   ▒█████   ██▓  ██████  ▒█████   ███▄    █ 
+ ▓██░  ██▒▒██▒  ██▒▓██▒▒██    ▒ ▒██▒  ██▒ ██ ▀█   █ 
+ ▓██░ ██▓▒▒██░  ██▒▒██▒░ ▓██▄   ▒██░  ██▒▓██  ▀█ ██▒
+ ▒██▄█▓▒ ▒▒██   ██░░██░  ▒   ██▒▒██   ██░▓██▒  ▐▌██▒
+ ▒██▒ ░  ░░  ████▓▒░░██░▒██████▒▒░ ████▓▒░▒██░  ▓██░
+ ▒▓▒░ ░  ░░ ▒░▒░▒░ ░▓  ▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ ░ ▒░   ▒ ▒ 
+ ░▒ ░       ░ ▒ ▒░  ▒ ░░ ░▒  ░ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░
+ ░░       ░ ░ ░ ▒   ▒ ░░  ░  ░  ░ ░ ░ ▒     ░   ░ ░ 
+             ░ ░   ░        ░      ░ ░           ░ 
+'''+W+''' ---------------------------------------------------
+'''+wd+'''   ➣ Author   : Zed Coder
+     ➣ GitHub   : https://github.com/968hacker
+     ➣ YouTube  : Zed cracker
+     ➣ telegram : https://t.me/zed_cracker_1
+ '''+W+'''---------------------------------------------------'''
+print(logo1)
+print(' 0770,0771,0772,0773,0750,0751\n 0752,0753,0754,0780,0781,0782 ..')
+so=input("\n   saratakay: ")
 def iii():
 	import random,sys,os
 	for aa in range(7000):
 		asia=random.randint(1000000, 9999999)
-		asia1=random.randint(1000000, 9999999)
-		asia2=random.randint(1000000, 9999999)
-		asia3=random.randint(1000000, 9999999)
-		asia4=random.randint(1000000, 9999999)
-		korak=random.randint(1000000, 9999999)
-		korak1=random.randint(1000000, 9999999)
-		korak2=random.randint(1000000, 9999999)
-		korak3=random.randint(1000000, 9999999)
-		korak4=random.randint(1000000, 9999999)
-		zain=random.randint(1000000, 9999999)
-		zain1=random.randint(1000000, 9999999)
-		zain2=random.randint(1000000, 9999999)
-		zain3=random.randint(1000000, 9999999)
-		zain4=random.randint(1000000, 9999999)
 		sys.stdout=open("combo.txt","a")
-		print("+964770"+str(asia1)+":0770"+str(asia1))
-		print("+964771"+str(asia2)+":0771"+str(asia2))
-		print("+964750"+str(korak)+":0750"+str(korak))
-		print("+964751"+str(korak1)+":0751"+str(korak))
+		print("+964"+str(so)+str(asia1)+":0770"+str(asia1))
 		sys.stdout.flush()
 iii()
 pass
